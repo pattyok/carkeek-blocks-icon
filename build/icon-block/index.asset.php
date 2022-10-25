191ec0053dcc9e4ad8245ba897836dc789e6c5db
pattyok <patty.ohara@gmail.com>
2022-10-25 14:14:07 -0700

    updating the block version

--- build/icon-block/index.asset.php
+++ build/icon-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '8e625951106cd7fa3721');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'e7ec6e31fa8fb229e9ed');
--- carkeek-blocks-icon.php
+++ carkeek-blocks-icon.php
@@ -7,7 +7,7 @@
  * Primary Branch: main
  * Requires at least: 5.9
  * Requires PHP:      7.0
- * Version:           1.0.02
+ * Version:           1.0.03
  * Author:            The Rhizome Collaborative
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
