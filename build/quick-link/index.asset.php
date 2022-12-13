0a4a134be3f772717a40bae9e0a11fc37ce3dc54
pattyok <patty.ohara@gmail.com>
2022-12-13 08:53:55 -0800

    update to mobile icon style

--- build/quick-link/index.asset.php
+++ build/quick-link/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f344d18688ec2bb9c42b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '103f45ce462c7d3e8905');
