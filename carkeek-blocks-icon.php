acfc4084bccac15a02067041b66ea159b2bff987
pattyok <patty.ohara@gmail.com>
2023-01-19 15:57:49 -0800

    fix to icon sizing

--- carkeek-blocks-icon.php
+++ carkeek-blocks-icon.php
@@ -7,7 +7,7 @@
  * Primary Branch: main
  * Requires at least: 5.9
  * Requires PHP:      7.0
- * Version:           1.0.07
+ * Version:           1.0.09
  * Author:            The Rhizome Collaborative
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
